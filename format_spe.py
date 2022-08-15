178d3e76864326d770dc821dcaf5e6bb1552cea5
krupadav <107058808+krupadav@users.noreply.github.com>
2022-08-15 19:57:07 +0530

    Add files via upload

---
+++ format_spe.py
@@ -0,0 +1,9 @@
+code = 8
+city = 'Mumbai'
+print('The city is',city)
+print('The code is', code)
+print('the city is {} and code is {}'.format(city,code))
+print('The city is {1} and the code is {0}'.format(code,city))
+print('The city is {} and the code is {}'.format(code,city))
+print('city is %s and code is %d'%(city,code))
+#but in format specifiers we need exactly same argument as in input.
